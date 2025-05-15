dffb73aa227a0ecc8a45c0429d415ff224fec494
Nong <naong69@gmail.com>
2025-05-15 15:25:21 +0700

    session08

---
+++ backend/routes/users.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const userService = require('../services/userServices');
+
+router.post('/login', (req, res) => {
+    const response = userService.login(req.body)
+})
+
+router.post('/register', (req, res) => {
+    const response = userService.register(req.body)
+})
+
+module.exports = router;
---
+++ backend/services/userServices.js
@@ -0,0 +1,17 @@
+
+
+function register(data){
+
+    return ff
+}
+
+function login(data){
+
+    return dd
+}
+
+module.exports = {
+    register,
+    login
+};
+
